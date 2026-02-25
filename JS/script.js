ad3e04c11120b39e9df122c4f7be528727ca4f95
VINSMOKE-69 <tagam.abner77@gmail.com>
2026-02-25 08:55:51 +0800

    WSAT-M4: JavaScript interactivity added

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,30 @@
-console.log("JS/script.js")
-
-
 const themeToggle = document.getElementById("themeToggle");
 
+window.addEventListener("DOMContentLoaded", () => {
+  if (document.body.classList.contains("dark-mode")) {
+ 
+    document.body.classList.remove("dark-mode");
+  }
+  themeToggle.textContent = "Dark Mode";
+  themeToggle.style.backgroundColor = "#4b3621";
+  themeToggle.style.color = "#e8d0b6b7"; 
+});
+
+
 themeToggle.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("dark-mode")) {
-        themeToggle.textContent = "Light Mode";
-        themeToggle.style.backgroundColor = "#e8d0b6b7";
-        themeToggle.style.color = "#4b3621";
-    } else {
-        themeToggle.textContent = "Dark Mode";
-        themeToggle.style.backgroundColor = "#4b3621";
-        themeToggle.style.color = "#e8d0b6b7";
-    }
+  if (document.body.classList.contains("dark-mode")) {
+    themeToggle.textContent = "Light Mode";
+    themeToggle.style.backgroundColor = "#e8d0b6b7";
+    themeToggle.style.color = "#4b3621"; 
+  } else {
+   
+    themeToggle.textContent = "Dark Mode";
+    themeToggle.style.backgroundColor = "#4b3621"; 
+    themeToggle.style.color = "#e8d0b6b7"; 
+  }
 });
-
-
-const toggleSkills = document.getElementById("toggleSkills");
-const skillsSection = document.getElementById("skillsSection");
-
-toggleSkills.addEventListener("click",function(){
-    skillsSection.classList.toggle("hidden");
-});
-
 
 submitBtn.addEventListener("click",function(event){
     event.preventDefault();
